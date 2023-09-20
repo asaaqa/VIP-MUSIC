78c0606999b4c9080ce2eb1b4b2bccd563117296
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-09-21 01:22:40 +0300

    Update start.py

--- VipX/plugins/start.py
+++ VipX/plugins/start.py
@@ -33,7 +33,7 @@ loop = asyncio.get_running_loop()
 
 
 @app.on_message(
-    filters.command(get_command(["start","sert" ]))
+    filters.command(get_command("START_COMMAND"))
     & filters.private
     & ~filters.edited
     & ~BANNED_USERS
