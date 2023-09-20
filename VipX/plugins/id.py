e458120a1664608f1d5a8506b63179a0c5be115e
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-09-21 00:26:50 +0300

    Update id.py

--- VipX/plugins/id.py
+++ VipX/plugins/id.py
@@ -26,7 +26,7 @@ def get_id(msg: Message):
                 return obj
 
 
-@app.on_message(filters.command(["ايدي","ا"],prefixes= [""]))
+@app.on_message(filters.command(["ايدي الجروب","iid"],prefixes= [""]))
 async def showid(_, message: Message):
     chat_type = message.chat.type
 
