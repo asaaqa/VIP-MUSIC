86543a5149912832b04ad425c4c08157a146e48f
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-04 17:07:24 +0300

    Update play.py

--- VipX/utils/inline/play.py
+++ VipX/utils/inline/play.py
@@ -219,6 +219,12 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
 
             ),
           ],
+            [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
           [
             InlineKeyboardButton(
 
